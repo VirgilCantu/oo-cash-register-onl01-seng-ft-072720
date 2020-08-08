73b7bcde39f73452dab37bab5f0d766582ece1ed
Virgil Cantu <cantu.sampsom@gmail.com>
2020-08-08 19:07:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -32,7 +32,7 @@ def apply_discount
 end
 
 def void_last_transaction
-  self.last_transaction[0] * self.last_transaction[2]
+  (self.last_transaction[0]) * (self.last_transaction[2])
   binding.pry
 
 
