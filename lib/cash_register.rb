3a167ae6a2e2944e5c299383350d02fa5d464d83
Virgil Cantu <cantu.sampsom@gmail.com>
2020-08-08 17:32:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+require 'pry'
 
+
+class CashRegister
+attr_accessor :total, :items
+
+
+def initialize
+  @total = 0
+end
+
+def add_item(title, price, quantity = 1)
+  self.total + price
+end
+binding.pry
+end
