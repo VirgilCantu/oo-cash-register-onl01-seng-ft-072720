036724083819dab4893e3228c2c20f4c5bd85460
Virgil Cantu <cantu.sampsom@gmail.com>
2020-08-08 18:54:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,9 @@ end
 def add_item(title, price, quantity = 1)
   self.items.concat([title] * quantity)
   self.total = self.total.to_f + price * quantity
-  
+  self.last_transaction[0] = title
+  self.last_transaction[1] = price
+  self.last_transaction[2] = quantity
 end
 
 def apply_discount
